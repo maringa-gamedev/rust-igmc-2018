3810efaf759ae9af232890531fcca19adb146698
Tiago Nascimento <tiago@theosirian.com>
2018-11-12 23:41:31 -0200

    ci try fix 2
    Signed-off-by: Tiago Nascimento <tiago@theosirian.com>

--- ci/install.sh
+++ ci/install.sh
@@ -42,8 +42,6 @@ main() {
            --git japaric/cross \
            --tag $tag \
            --target $target
-
-    sudo apt install libasound2-dev libx11-xcb-dev libssl-dev cmake libfreetype6-dev libexpat1-dev libxcb1-dev
 }
 
 main
